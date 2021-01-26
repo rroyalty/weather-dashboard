a2e484fd7a29c88d018cb0acacd6d378a44bbad6
rroyalty <rroyalty@gmail.com>
2021-01-26 12:16:16 -0500

    Current City

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ $(document).ready(function() {
     const weatherKey = "dc35c92a93f41b8b8d2a3cb13f096b47"
     const weatherURL = "https://api.openweathermap.org/data/2.5/onecall?"     // lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
-    const citiesKey = "6fa477f4e23440d282eae33f025aa4c9"
+    const citiesKey = "&key=6fa477f4e23440d282eae33f025aa4c9"
     const citiesURL = "https://api.opencagedata.com/geocode/v1/json?q="  // q=PLACENAME&key=YOUR-API-KEY
 
     let currentLat = "NAN"
     let currentLng = "NAN"
+    let currentCity = ""
 
-    navigator.geolocation.getCurrentPosition(function(position) {
-        while(currentLat === "NAN" || currentLng === "NAN") {
-            currentLat = position.coords.latitude;
-            currentLng = position.coords.longitude;
-        }
-    });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            while(currentLat === "NAN" || currentLng === "NAN") {
+                currentLat = position.coords.latitude;
+                currentLng = position.coords.longitude;
+            }
+            
+            currentCity = $.get( citiesURL + currentLat + "," + currentLng + citiesKey + "&no_annotations=1", function(data) {console.log(data)});
 
-    const currentCity = $.get( citiesURL + "Boston&key=" + citiesKey + "&no_annotations=1", function(data) { 
-
-    });
+        }); } else {alert("Location could not be retrieved.")};
 
     startClock();
 
@@ -32,13 +33,13 @@ $(document).ready(function() {
             dtDisplay.text(dtInit);
             dtInit = dt.local().weekdayLong  + ", " + dt.local().plus({seconds: 1}).toLocaleString(dt.DATETIME_MED_WITH_SECONDS);
         }, 1000);
-    }
+    };
  
-    let weatherData = $.get( citiesURL + "Boston&key=" + citiesKey + "&no_annotations=1", function(data) {
+    let weatherData = $.get( citiesURL + "Boston" + citiesKey + "&no_annotations=1", function(data) {
         $.get( weatherURL + "lat=" + data.results[0].geometry.lat + "&lon="  + data.results[0].geometry.lng + "&appid=" + weatherKey, function(data2) {
             return data2;
-        })
-    })
+        });
+    });
 
 });
 
